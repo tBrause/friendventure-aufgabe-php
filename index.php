44341d23de48957e23f97d494c83540f67b045f7
tBrause <brause.torsten@gmail.com>
2022-05-10 05:25:06 +0200

    ch title

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ if ($showid !== '') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>EinkaufslisteApp</title>
     <style>
         body {
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
